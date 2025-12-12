15e237eda115730018c60e709aa84c4bafd925c2
ControlPlane CI <ci@controlplane.com>
2025-12-12 19:11:17 +0200

    Add GitHub release creation after successful publish

--- .github/scripts/slack-notify.sh
+++ .github/scripts/slack-notify.sh
@@ -12,7 +12,7 @@ fi
 
 curl -X POST -H 'Content-type: application/json' --data @- "${SLACK_NOTIFY_URL}" <<EOF
 {
-  "text":"${GITHUB_ACTOR} has run *${GITHUB_REF_NAME}* in *${GITHUB_REPOSITORY}*: ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+  "text":"${GITHUB_ACTOR} has run Publish to NPM in *${GITHUB_REPOSITORY}*: ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 }
 EOF
 
